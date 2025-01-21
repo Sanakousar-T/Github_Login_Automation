2050a3567df51d3816b4546dcebe303b09740d25
Sanakousar <sanakousar.tallihal@gmail.com>
2025-01-21 17:28:53 +0530

    3st cammit

--- github_login.py
+++ github_login.py
@@ -1,9 +1,9 @@
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys 
+from selenium.webdriver.common.by import By 
+from selenium.webdriver.chrome.service import Service 
+from webdriver_manager.chrome import ChromeDriverManager 
 import time
 
 # Initialize the Chrome WebDriver
